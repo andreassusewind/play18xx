837e40b3cef9dd0f38a27fa2df4ce663f59609a8
Andreas Susewind <andreas@amsusewind.de>
2018-11-15 22:31:43 +0100

    first commit

---
+++ play18xx/src/main/java/com/play18xx/app/App.java
@@ -0,0 +1,12 @@
+package com.play18xx.app;
+
+public class App 
+{
+	public static boolean verbose = true;
+	public static String gamename = "Play18xx";
+	
+	public static void main( String[] args )
+    {
+        System.out.println( "Starting Application - play18xx" );
+    }
+}
